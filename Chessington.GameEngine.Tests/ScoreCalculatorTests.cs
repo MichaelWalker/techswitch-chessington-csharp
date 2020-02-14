be366768079675bc103e581dd303b175d68c4d24
Mike Walker <michael.walker@softwire.com>
2020-02-14 06:47:41 +0000

    Returns 0 if no pieces are captured.

---
+++ Chessington.GameEngine.Tests/ScoreCalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Chessington.GameEngine.Pieces;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Chessington.GameEngine.Tests
+{
+    [TestFixture]
+    public class ScoreCalculatorTests
+    {
+        [Test]
+        public void GetWhiteScore_ReturnsZeroIfNoPiecesAreCaptured()
+        {
+            var board = A.Fake<IBoard>();
+            var scoreCalculator = new ScoreCalculator(board);
+            A.CallTo(() => board.CapturedPieces).Returns(new List<Piece>());
+
+            var whiteScore = scoreCalculator.GetWhiteScore();
+
+            whiteScore.Should().Be(0);
+        }
+        
+        [Test]
+        public void GetBlackScore_ReturnsZeroIfNoPiecesAreCaptured()
+        {
+            var board = A.Fake<IBoard>();
+            var scoreCalculator = new ScoreCalculator(board);
+            A.CallTo(() => board.CapturedPieces).Returns(new List<Piece>());
+
+            var whiteScore = scoreCalculator.GetWhiteScore();
+
+            whiteScore.Should().Be(0);
+        }
+    }
+}
